cb9a4989ddfceadf715c89448360a71ca6ce8bcd
Ashenafi Tilahun <ashenafig2012@outlook.com>
2022-04-18 15:07:43 -0700

    First Commit

---
+++ Customers.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Customers]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(20) NULL, 
+    [Website] VARCHAR(25) NULL
+)
---
+++ Restore_Adv_DB.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[Restore_Adv_DB]
+	
+AS
+
+RESTORE DATABASE [AdventureWorks2019] FROM  DISK = N'D:\SQL Databse backup and Files\AdventureWorks2019.bak' 
+WITH  FILE = 1,  MOVE N'AdventureWorks2017' TO N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\AdventureWorks2019.mdf',  MOVE N'AdventureWorks2017_log' TO N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\AdventureWorks2019_log.ldf',  NOUNLOAD,  REPLACE,  STATS = 5
+
+GO
+
+
+
+	
